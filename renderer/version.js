201d7dd851e67174df3111c22620f1a63b037732
SkyHigh Modifications <fordp899@gmail.com>
2026-06-03 12:45:47 +0100

    Remove application source from repository

--- renderer/version.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const versionElement = document.getElementById("version");
-    window.api.onVersion((version) => {
-        if (versionElement) {
-            versionElement.textContent = version;
-        }
-    });
-});   
